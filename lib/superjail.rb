e66edb2652b34fed3fe359db78486f96decd5120
winton <mail@wintoni.us>
2009-01-11 03:22:18 -0800

    Fixes from testing

--- lib/superjail.rb
+++ lib/superjail.rb
@@ -1,22 +1,20 @@
 require 'erb'
-$:.unshift File.dirname(__FILE__)
 
 module Superjail
   Version = "0.1.0"
   
   module Helpers
-    Debug = true
+    Debug = false
     
     # Execute a command and display the output
     def run(cmd)
-      puts cmd
+      puts "\nRunning command:\n#{cmd}\nOutput:\n"
       puts `#{cmd}` unless Debug
     end
     
     # Write text to a file
     def write(path, text, mode='a')
-      puts "Writing to #{path}:"
-      puts text
+      puts "\nWriting to #{path}:\n#{text}"
       File.open(path, mode) { |f| f.write(text) } unless Debug
     end
   end
@@ -34,7 +32,7 @@ module Superjail
         # Install jailkit
         name = File.basename(url).gsub('.tar.gz', '')
         run "tar xzf #{name}*"
-        run "cd #{name} && make configure && ./configure --prefix=/usr/local && make && sudo make install"
+        run "cd #{name} && make configure && ./configure && make && sudo make install"
         run "rm -Rf #{name}"
         run "rm -Rf #{name}*"
       end
@@ -45,15 +43,11 @@ module Superjail
     class <<self
       include Helpers
       
-      # Executes /usr/local/bin/jk_init
+      # Executes /usr/bin/jk_init
       def create(jail, options={})
-        # Templates
-        custom_section = ERB.new(File.read("../templates/custom_section.erb"))
-        jk_lsh_section = ERB.new(File.read("../templates/jk_lsh_section.erb"))
-        jk_lsh_config  = ERB.new(File.read("../templates/jk_lsh_config.erb"))
         # Default options
         options = {
-          :config      => '/usr/local/etc/jailkit/jk_init.ini',
+          :config      => '/etc/jailkit/jk_init.ini',
           :force       => true,
           :hardlink    => false,
           :sections    => [],
@@ -61,14 +55,16 @@ module Superjail
           :temp_config => '/tmp/jk_init.ini',
           :verbose     => true
         }.merge(options)
+        # Templates
+        templates = "#{File.dirname(__FILE__)}/../templates/"
+        custom_section = ERB.new(File.read("#{templates}custom_section.erb"))
+        jk_lsh_config  = ERB.new(File.read("#{templates}jk_lsh_config.erb"))
         # Copy config to temp_config
-        FileUtils.cp options[:config], options[:temp_config]
+        FileUtils.cp(options[:config], options[:temp_config]) unless Debug
         # If custom shell
         if options[:shell]
-          # Add jk_lsh_section to temp_config
-          write options[:temp_config], jk_lsh_section.result(binding)
-          # Add custom_jk_lsh to sections list
-          options[:sections] << :custom_jk_lsh
+          # Add jk_lsh to sections list
+          options[:sections] << :jk_lsh
         end
         # Search for custom sections (hashes)
         options[:sections].each_index do |index|
@@ -90,15 +86,15 @@ module Superjail
         # Throw exception if no jail or sections?
         params.push('-j ' + jail)
         params.push(options[:sections].join(' '))
-        # Execute /usr/local/bin/jk_init call
+        # Execute /usr/sbin/jk_init call
         run "jk_init #{params.join(' ')}"
         # If custom shell, create jk_lsh_config inside the jail
         if options[:shell]
           config = options[:shell]
           path = jail + '/etc/jailkit/jk_lsh.ini'
-          run "mkdir -p #{File.basename path}"
+          run "mkdir -p #{File.dirname path}"
           run "touch #{path}"
-          write path, jk_lsh_config.result(binding)
+          write path, jk_lsh_config.result(binding), 'w'
         end
       end
     end
@@ -107,26 +103,21 @@ module Superjail
       class <<self
         include Helpers
         
-        # Create user and jail
-        def create(user, options={})
+        # Create and jail user
+        def create(jail, group, user, password)
+          run "groupadd -f #{group}"
+          run "useradd -g #{group} -p #{password} #{user}"
+          run "jk_jailuser -n -v -m -j #{jail} #{user}"
+          # Add user to limits.conf
         end
         
         # Execute command in jail as user
-        # Create user and jail if either do not exist
-        def execute_as(user, options={})
-        end
-        
-        # Execute /usr/local/bin/jk_jailuser
-        def jail(user, options={})
-          options = {
-            :jail      => '/jail',
-            :verbose   => true,
-            :verbose   => true,
-            :shell     => '/usr/sbin/jk_lsh',
-            :move_home => true
-          }.merge(options)
-          # Generate command and execute
-          #   Always run with --noninteractive
+        # Create user if does not exist
+        def execute_as_jailed_user(cmd, jail, group, user, password)
+          if `grep "^#{user}:" /etc/passwd`.empty?
+            create jail, group, user, password
+          end
+          run cmd
         end
       end
     end
