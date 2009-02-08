cea1c4a850ebe33ce13c3cac2f940b1646caa515
winton <mail@wintoni.us>
2009-02-07 19:47:49 -0800

    Updates

--- lib/superjail.rb
+++ lib/superjail.rb
@@ -4,12 +4,12 @@ module Superjail
   Version = "0.1.0"
   
   module Helpers
-    Debug = false
+    Debug = true
     
     # Assign default options and bind as local vars
-    def default_options(default={}, options={}, binding)
+    def default_options(default, options, bind)
       options = default.merge(options)
-      options.each { |key, value| eval("#{key} = #{value.inspect}", binding) }
+      options.each { |key, value| eval("@#{key} = #{value.inspect}", bind) }
     end
     
     # Execute a command and display the output
@@ -52,52 +52,53 @@ module Superjail
       include Helpers
       
       # Executes jk_init
-      def create(jail, options={})
+      def create(options={})
         # Default options
-        default_options {
+        default_options({
           :config      => '/etc/jailkit/jk_init.ini',
           :force       => true,
           :hardlink    => false,
+          :jail        => '/jail',
           :sections    => [],
           :shell       => nil,
           :temp_config => '/tmp/jk_init.ini',
           :verbose     => true
-        }, options, binding
+        }, options, binding)
         # Templates
         templates = "#{File.dirname(__FILE__)}/../templates/"
         custom_section = ERB.new(File.read("#{templates}custom_section.erb"))
         jk_lsh_config  = ERB.new(File.read("#{templates}jk_lsh_config.erb"))
         # Copy config to temp_config
-        FileUtils.cp(config, temp_config) unless Debug
+        FileUtils.cp(@config, @temp_config) unless Debug
         # If custom shell
-        if shell
+        if @shell
           # Add jk_lsh to sections list
-          sections << :jk_lsh
+          @sections << :jk_lsh
         end
         # Search for custom sections (hashes)
-        sections.each_index do |index|
-          section = sections[index]
+        @sections.each_index do |index|
+          section = @sections[index]
           # If sections contains a hash, it defines a custom section
           if section.respond_to?(:keys)
             # Append custom_section to temp_config
-            write temp_config, custom_section.result(binding)
+            write @temp_config, custom_section.result(binding)
             # Replace hash with name so it is included in the final jk_init call
-            sections[index] = section[:name]
+            @sections[index] = section[:name]
           end
         end
         # Build call parameters
         params = []
-        params.push('-v') if verbose
-        params.push('-f') if force
-        params.push('-k') if hardlink
-        params.push('-c ' + temp_config)
-        params.push('-j ' + jail)
-        params.push(sections.join(' '))
+        params.push('-v') if @verbose
+        params.push('-f') if @force
+        params.push('-k') if @hardlink
+        params.push('-c ' + @temp_config)
+        params.push('-j ' + @jail)
+        params.push(@sections.join(' '))
         # Execute /usr/sbin/jk_init call
         run "jk_init #{params.join(' ')}"
         # If custom shell, create jk_lsh_config inside the jail
         if shell
-          path = jail + '/etc/jailkit/jk_lsh.ini'
+          path = @jail + '/etc/jailkit/jk_lsh.ini'
           run "mkdir -p #{File.dirname path}"
           run "touch #{path}"
           write path, jk_lsh_config.result(binding), 'w'
@@ -117,18 +118,18 @@ module Superjail
         # Create and jail user
         def create(options={})
           # Default options
-          default_options {
+          default_options({
             :jail     => '/jail',
             :group    => 'jailed',
             :user     => 'jailed',
             :password => 'lockmeup'
-          }, options, binding
+          }, options, binding)
           # Create group
-          run "groupadd -f #{group}"
+          run "groupadd -f #{@group}"
           # Create user
-          run "useradd -m -g #{group} -p `mkpasswd #{password}` #{user}"
+          run "useradd -m -g #{@group} -p `mkpasswd #{@password}` #{@user}"
           # Move user to jail
-          run "jk_jailuser -n -v -m -j #{jail} #{user}"
+          run "jk_jailuser -n -v -m -j #{@jail} #{@user}"
           # TODO: Add group to limits.conf (if not already present)
         end
         
